2aae88917d0d188a5b6528cc1f22e4b93fb360ca
Svetlana Sieber <135451084+svetlanasieber@users.noreply.github.com>
2025-02-15 20:19:17 +0100

    Update Sensor.java

--- main/java/TirePressureMonitoringSystem/Sensor.java
+++ main/java/TirePressureMonitoringSystem/Sensor.java
@@ -1,10 +1,8 @@
-package p06_TirePressureMonitoringSystem;
+package TirePressureMonitoringSystem;
 
 import java.util.Random;
 
 public class Sensor {
-    // The reading of the pressure value from the sensor is simulated in this implementation.
-    // Because the focus of the exercise is on the other class.
 
     private static final double OFFSET = 16;
 
@@ -16,7 +14,6 @@ public class Sensor {
     }
 
     private double readPressureSample() {
-        // Simulate info read from a real sensor in a real tire
         return 6 * randomPressureSampleSimulator.nextDouble() * randomPressureSampleSimulator.nextDouble();
     }
 }
